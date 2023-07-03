7ff141d2f1002283e389e9af5e31d2fc58cb7187
vd <vd@>
2023-07-03 23:57:14 +0300

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import setuptools
 
 setuptools.setup(
     name='snowflake-id',
-    version='0.0.3',
+    version='0.0.4',
     author='Vd',
     author_email='vd@vd2.org',
     url='https://github.com/vd2org/snowflake',
